988611298338370e851cd843f8ada034954fd7bb
Leuwur <evadevizcarra@gmail.com>
2020-02-12 13:44:05 -0800

    eva

---
+++ binarionum.py
@@ -0,0 +1,9 @@
+print("Presiona 'x' para salir");
+binario = input("Ingrese un numero en binario: ");
+if binario == 'x':
+  exit();
+
+else:
+     decimal = int(binario, 2);
+     print(binario, "En decimal =", decimal);
+
